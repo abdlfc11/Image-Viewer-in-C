aff24c87c85964cd42455f3f8aaf13476fe4f0b8
Abdulrahman Sultan <119352671+abdlfc11@users.noreply.github.com>
2026-06-07 10:08:12 +0100

    Initial commit

---
+++ src.c
@@ -0,0 +1,8 @@
+#include<stdio.h> /* standard input output library, provides printf, scanf etc */
+#include<stdlib.h>
+
+int main(void) { /* excplicitely shows no given args */
+    printf("Hello World! \n");
+
+    return 0; /* success return */
+}
